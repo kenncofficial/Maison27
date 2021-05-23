59db5978c9c273ea38adc8ced721ed79cfb90a4b
kenneth Chinedu <chineduken17@gmail.com>
2021-05-23 22:10:35 +0100

    NEC3

--- Maison27/urls.py
+++ Maison27/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('maison_auth/', include('maison_auth.urls')),
 ] 
 
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
