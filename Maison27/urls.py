44e28b762cfb849c837863eac409944e077b99fe
kenneth Chinedu <chineduken17@gmail.com>
2021-05-23 22:04:51 +0100

    new error check 2

--- Maison27/urls.py
+++ Maison27/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
    	path('', include('Blog.urls')),
    	path('maison_auth/', include('django.contrib.auth.urls')),
     path('maison_auth/', include('maison_auth.urls')),
-
 ] 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
